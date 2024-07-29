b9488840339b43dd6a4afe810b72d5719f27b058
Anmol Singh <Anmol_singh@live.no>
2024-07-29 09:13:52 +0200

    Split training into seperate file

---
+++ train.py
@@ -0,0 +1,94 @@
+from transformer import TransformerLanguageModel
+import torch
+
+# Hyperparameters
+batch_size = 64  # Number of independent training examples to train on in parallell
+block_size = 128  # Maximum context length for predictions
+max_iters = 6000
+eval_interval = 500
+learning_rate = 3e-4
+device = "mps" if torch.backends.mps.is_available() else "cpu" # As I run this on a MacBook M2 Pro chip, I can use MPS to speed up matrix multiplications
+print("Using device: " + device)
+eval_iters = 200
+n_embd = 128 # Number of dimensions in embedding space
+n_head = 4
+n_layer = 6 # Number of Blocks
+dropout = 0.3
+# ----------------
+
+torch.manual_seed(1337) # Same seed as used in Karpathy's tutorial video
+
+# !wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
+
+# Open file and read it
+with open("hhgttg.txt", "r", encoding="utf-8") as f:
+    text = f.read()
+
+# Get all tokens (int our case all individual characters)
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+
+# Create mapping from chars to ints and back
+stoi = { ch:i for i, ch in enumerate(chars)}
+itos = { i:ch for i, ch in enumerate(chars)}
+encode = lambda s: [stoi[c] for c in s]
+decode = lambda l: "".join([itos[i] for i in l])
+
+# Train and test splits
+data = torch.tensor(encode(text), dtype=torch.long)
+n = int(0.9*len(data))
+train_data = data[:n]
+val_data = data[n:]
+
+# Data loading 
+def get_batch(split):
+    data = train_data if split == "train" else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,)) # Generate batch_size number of random indices
+    x = torch.stack([data[i: i + block_size] for i in ix])
+    y = torch.stack([data[i + 1: i + block_size + 1] for i in ix])
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ["train", "val"]:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+model = TransformerLanguageModel(vocab_size, block_size, device, n_embd, n_head, n_layer, dropout)
+m = model.to(device)
+
+# Create optimizer
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+# Train the model
+for iter in range(max_iters):
+
+    if iter % eval_interval == 0 or iter == max_iters- 1:
+        losses = estimate_loss()
+        print(f"Step {iter}: train loss {losses["train"]:.4f}, val loss {losses["val"]:.4f}")
+
+    # Sample a batch of data
+    xb, yb = get_batch("train")
+
+    # Evaluate loss
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+
+    # Calculate gradients based on loss
+    loss.backward()
+
+    # Optimize neural network based on gradients
+    optimizer.step()
+
+# Generate from the model
+context = torch.zeros((1, 1), dtype=torch.long, device=device)
+print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
--- transformer.py
+++ transformer.py
@@ -2,72 +2,10 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-# Hyperparameters
-batch_size = 64  # Number of independent training examples to train on in parallell
-block_size = 128  # Maximum context length for predictions
-max_iters = 6000
-eval_interval = 500
-learning_rate = 3e-4
-device = "mps" if torch.backends.mps.is_available() else "cpu" # As I run this on a MacBook M2 Pro chip, I can use MPS to speed up matrix multiplications
-print("Using device: " + device)
-eval_iters = 200
-n_embd = 128 # Number of dimensions in embedding space
-n_head = 4
-n_layer = 6 # Number of Blocks
-dropout = 0.3
-# ----------------
-
-torch.manual_seed(1337) # Same seed as used in Karpathy's tutorial video
-
-# !wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-
-# Open file and read it
-with open("hhgttg.txt", "r", encoding="utf-8") as f:
-    text = f.read()
-
-# Get all tokens (int our case all individual characters)
-chars = sorted(list(set(text)))
-vocab_size = len(chars)
-
-# Create mapping from chars to ints and back
-stoi = { ch:i for i, ch in enumerate(chars)}
-itos = { i:ch for i, ch in enumerate(chars)}
-encode = lambda s: [stoi[c] for c in s]
-decode = lambda l: "".join([itos[i] for i in l])
-
-# Train and test splits
-data = torch.tensor(encode(text), dtype=torch.long)
-n = int(0.9*len(data))
-train_data = data[:n]
-val_data = data[n:]
-
-# Data loading 
-def get_batch(split):
-    data = train_data if split == "train" else val_data
-    ix = torch.randint(len(data) - block_size, (batch_size,)) # Generate batch_size number of random indices
-    x = torch.stack([data[i: i + block_size] for i in ix])
-    y = torch.stack([data[i + 1: i + block_size + 1] for i in ix])
-    x, y = x.to(device), y.to(device)
-    return x, y
-
-@torch.no_grad()
-def estimate_loss():
-    out = {}
-    model.eval()
-    for split in ["train", "val"]:
-        losses = torch.zeros(eval_iters)
-        for k in range(eval_iters):
-            X, Y = get_batch(split)
-            logits, loss = model(X, Y)
-            losses[k] = loss.item()
-        out[split] = losses.mean()
-    model.train()
-    return out
-
 class Head(nn.Module):
     """One Head of self-attention"""
 
-    def __init__(self, head_size) -> None:
+    def __init__(self, block_size, head_size, n_embd, dropout) -> None:
         super().__init__()
         self.key = nn.Linear(n_embd, head_size, bias=False)
         self.query = nn.Linear(n_embd, head_size, bias=False)
@@ -95,9 +33,9 @@ class Head(nn.Module):
 class MultiHeadAttention(nn.Module):
     """Multiple heads of self-attention in parallel"""
 
-    def __init__(self, num_heads, head_size) -> None:
+    def __init__(self, block_size, num_heads, head_size, n_embd, dropout) -> None:
         super().__init__()
-        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.heads = nn.ModuleList([Head(block_size, head_size, n_embd, dropout) for _ in range(num_heads)])
         self.proj = nn.Linear(n_embd, n_embd)
         self.dropout = nn.Dropout(dropout)
     
@@ -108,7 +46,7 @@ class MultiHeadAttention(nn.Module):
 
 class FeedForward(nn.Module):
 
-    def __init__(self, n_embd) -> None:
+    def __init__(self, n_embd, dropout) -> None:
         super().__init__()
         self.net = nn.Sequential(
             nn.Linear(n_embd, 4 * n_embd),
@@ -123,11 +61,11 @@ class FeedForward(nn.Module):
 class Block(nn.Module):
     """Transformer block: Communication followed by computation"""
 
-    def __init__(self, n_embd, n_head) -> None:
+    def __init__(self, block_size, n_embd, n_head, dropout) -> None:
         super().__init__()
         head_size = n_embd // n_head    # n_embd: embedding dimension, n_head: the number of heads we'd like to hav
-        self.sa = MultiHeadAttention(n_head, head_size)
-        self.ffwd = FeedForward(n_embd)
+        self.sa = MultiHeadAttention(block_size, n_head, head_size, n_embd, dropout)
+        self.ffwd = FeedForward(n_embd, dropout)
         self.ln1 = nn.LayerNorm(n_embd)
         self.ln2 = nn.LayerNorm(n_embd)
     
@@ -140,13 +78,16 @@ class Block(nn.Module):
 class TransformerLanguageModel(nn.Module):
     """A Language Model based on the Transformer architecture"""
 
-    def __init__(self) -> None:
+    def __init__(self, vocab_size, block_size, device, n_embd, n_head, n_layer, dropout) -> None:
         super().__init__()
-        
+
+        self.block_size = block_size
+        self.device = device
+
         # Each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.blocks = nn.Sequential(*[Block(block_size, n_embd, n_head, dropout) for _ in range(n_layer)])
         self.ln_f = nn.LayerNorm(n_embd)    # Final layer norm
         self.lm_head = nn.Linear(n_embd, vocab_size)
     
@@ -156,7 +97,7 @@ class TransformerLanguageModel(nn.Module):
 
         # idx and targets are both (B, T) - (Batch, Time), where time is input string - tensor of integers  
         tok_emb = self.token_embedding_table(idx) # (B, T, C) - C is same as n_embd - Embedding the tokens
-        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, C)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=self.device)) # (T, C)
         x = tok_emb + pos_emb # (B, T, C)
         x = self.blocks(x)  # (B, T, C)
         x = self.ln_f(x)    # (B, T, C)
@@ -179,7 +120,7 @@ class TransformerLanguageModel(nn.Module):
         for _ in range(max_new_tokens):
 
             # Crop idx to the last block_size tokens
-            idx_cond = idx[:, -block_size:]
+            idx_cond = idx[:, -self.block_size:]
 
             # get predictions
             logits, loss = self(idx_cond)
@@ -196,34 +137,3 @@ class TransformerLanguageModel(nn.Module):
             # Append sampled index to the running sequence, shape: (B, T+1)
             idx = torch.cat((idx, idx_next), dim=1)
         return idx
-
-
-model = TransformerLanguageModel()
-m = model.to(device)
-
-# Create optimizer
-optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
-
-# Train the model
-for iter in range(max_iters):
-
-    if iter % eval_interval == 0 or iter == max_iters- 1:
-        losses = estimate_loss()
-        print(f"Step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
-
-    # Sample a batch of data
-    xb, yb = get_batch("train")
-
-    # Evaluate loss
-    logits, loss = model(xb, yb)
-    optimizer.zero_grad(set_to_none=True)
-
-    # Calculate gradients based on loss
-    loss.backward()
-
-    # Optimize neural network based on gradients
-    optimizer.step()
-
-# Generate from the model
-context = torch.zeros((1, 1), dtype=torch.long, device=device)
-print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
