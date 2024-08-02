0703f6d0a1e1aa3d4b819d8345b678921a958313
Anmol Singh <Anmol_singh@live.no>
2024-08-02 09:13:30 +0200

    Add model trained on The Children of Hurin

--- train.py
+++ train.py
@@ -4,8 +4,8 @@ import torch
 
 # Hyperparameters
 batch_size = 64  # Number of independent training examples to train on in parallell
-block_size = 192  # Maximum context length for predictions
-max_iters = 9000
+block_size = 128  # Maximum context length for predictions
+max_iters = 5000
 eval_interval = 500
 learning_rate = 3e-4
 device = "mps" if torch.backends.mps.is_available() else "cpu" # As I run this on a MacBook M2 Pro chip, I can use MPS to speed up matrix multiplications
